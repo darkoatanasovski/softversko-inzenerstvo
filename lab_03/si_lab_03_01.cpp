5da9721888fcb94e184dafbb955404cc869dd006
darkoatanasovski <darkoatanasovski@gmail.com>
2012-11-25 20:06:32 +0100

    Направен е функциски, а не класен објект :)

--- lab_03/si_lab_03_01.cpp
+++ lab_03/si_lab_03_01.cpp
@@ -5,8 +5,7 @@
 #include <algorithm>
 using namespace std;
 
-class PowerOf2{
-public:
+
 	int power(int n)
 	{
 		if(n == 0 || n == 2)
@@ -21,24 +20,21 @@ public:
 		
 		return power(n/2);
 	}
-	bool operator() (int elem) 
+	bool PowerOf2(int elem) 
 	{
 		return power(elem)==0;
 	}
-};
-
 int _tmain(int argc, _TCHAR* argv[])
 {
 	vector<int> coll;
 	for(int i=1;i<10;i++)
 	{
-		PowerOf2 p2;
 		coll.push_back(i);
 	}
 	copy(coll.begin(),coll.end(),ostream_iterator<int>(cout, " "));
 	cout<<endl;
 
-	vector<int>::iterator iter = remove_if(coll.begin(),coll.end(),PowerOf2() );
+	vector<int>::iterator iter = remove_if(coll.begin(),coll.end(),PowerOf2 );
 
 	copy(coll.begin(), iter, ostream_iterator<int>(cout, " "));
 	cout<<endl;
